a0d5ae8ac23c60ba584f740617f6dee33a8146ab
Amit Mahor <50043061+amitmahor01@users.noreply.github.com>
2025-03-05 12:54:35 +0530

    removing project. Will create another from scratch

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="bg-primary min-h-screen">
-      <h1 className="text-4xl font-serif text-center p-8">Welcome to the Photographer Portfolio</h1>
-    </div>
-  );
-} 
